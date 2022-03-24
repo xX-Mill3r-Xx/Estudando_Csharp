d8cd6fd6652f5b7398de491c8951f6bb38bae4a4
Miller Vasco <millermartinsvasco1@gmail.com>
2022-03-24 15:33:05 -0300

    Add intro POO

---
+++ Introducao/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Introducao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             C# e .NET;
+
+            ¹ C# é uma linguagem de programação (regras sintaticas)
+
+            ² .NET (2022) é uma plataforma de desenvolvimento para se criar diversos tipos de aplicaçoes, podendo usar varias linguagens de programação;
+
+            o .NET basicamente tem dois componentes,
+            BCL - Base class Library
+              Bibliotecas de classe basicas; O .NET ja disponibiliza varias bibliotecas que podem ser usadas em nossas aplicaçoes;
+
+            CLR - Common Lenguage Runtime (Maquina Virtual)
+            Programas feitos em .NET rodam em cima do CLR(Maquina Virtual)
+             -Nota: possui garbage Collection (Objetos não utilizados são automaticamente desalocados da memoria)
+             */
+        }
+    }
+}
