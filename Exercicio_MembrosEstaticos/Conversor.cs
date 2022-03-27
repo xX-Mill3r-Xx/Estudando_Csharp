c539e6368b5a3c967c1c70c43378f4ac1c891243
Miller Vasco <millermartinsvasco1@gmail.com>
2022-03-27 15:14:28 -0300

    151427032022

---
+++ Exercicio_MembrosEstaticos/Conversor.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Exercicio_MembrosEstaticos
+{
+    static class Conversor
+    {
+        public static double IOF = 6.0;
+        public static double Dolar;
+        public static double Reais;
+
+        public static double valorDolar()
+        {
+            double total = (Dolar * Reais);
+            return total + (total * IOF / 100.0);
+        }
+    }
+}
---
+++ Exercicio_MembrosEstaticos/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_MembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Fazer um programa para ler a cotação do dolar, e depois um valor em dolar a ser comprado por uma 
+            pessoa em reais. Informar quantos reais a pessoa vai pagar pelos dolares, considerando ainda que 
+            a pessoa vat pagar 6% de IOF sobre o valor do dolar. Cria uma classe Conversor de Moedas para ser 
+            responsavel pelos calculos.
+             */
+
+            Console.Write("Qual a cotação do dolar? ");
+            Conversor.Dolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dolares voce vai comprar? ");
+            Conversor.Reais = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double total = double.Parse(Conversor.valorDolar().ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(total.ToString("F2", CultureInfo.InvariantCulture));
+            Console.ReadLine();
+        }
+    }
+}
