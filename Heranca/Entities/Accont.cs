15b50c4c09debe07cfbb7e1ad24b3e82822d168e
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-02 10:11:33 -0300

    101102042022

---
+++ Heranca/Entities/Accont.cs
@@ -0,0 +1,32 @@
+﻿
+
+namespace Heranca.Entities
+{
+    class Accont
+    {
+        public int Number { get; private set; }
+        public string Holder { get; private set; }
+        public double Balance { get; protected set; }
+
+        public Accont()
+        {
+        }
+
+        public Accont(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
---
+++ Heranca/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Heranca.Entities;
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Herança
+                • É um tipo de associação que permite que uma classe herde dados e 
+                comportamentos de outra
+                • Definições importantes
+                • Vantagens
+                • Reuso
+                • Polimorfismo
+                • Sintaxe
+                • : (estende)
+                • base (referência para a superclasse)
+            */
+
+            /*Exemplo
+                    Suponha um negócio de banco que possui uma conta comum e uma conta para 
+                    empresas, sendo que a conta para empresa possui todos membros da conta 
+                    comum, mais um limite de empréstimo e uma operação de realizar empréstimo.
+            */
+
+            /*Herança permite o reuso de atributos e 
+              métodos (dados e comportamento)*/
+
+            /*      • Relação "é-um"
+                    • Generalização/especialização
+                    • Superclasse (classe base) / subclasse (classe 
+                    derivada)
+                    • Herança / extensão
+                    • Herança é uma associação entre classes (e não 
+                    entre objetos)
+            */
+
+            /*Demo
+                Vamos implementar as classes
+                Account e BusinessAccount e fazer
+                alguns testes.
+            */
+
+            BusinessAccount account = new BusinessAccount(8010, "Bob Brown", 100.00, 500.0);
+
+            
+        }
+    }
+}
