4ab4c394b453595e4b59f453c85df9ba5fbf04c9
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-05 11:46:33 -0300

    114605042022

---
+++ Exercicio_ExcecaoFixaxao/Entities/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_ExcecaoFixaxao.Entities
+{
+    class Account
+    {
+        public int Number { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; set; }
+        public double WithDrawLimit { get; set; }
+
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance, double withDrawLimit)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+            WithDrawLimit = withDrawLimit;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void WithDraw(double amount)
+        {
+            if(Balance >= WithDrawLimit)
+            {
+                Console.WriteLine("Withdraw error: The amount exceeds withdraw limit");
+            }
+            else
+            {
+                Balance -= amount;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"New balance: ${Balance.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Exercicio_ExcecaoFixaxao/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Exercicio_ExcecaoFixaxao.Entities;
+using System.Globalization;
+
+namespace Exercicio_ExcecaoFixaxao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Exercício de fixação
+                Fazer um programa para ler os dados de uma conta bancária e depois realizar um 
+                saque nesta conta bancária, mostrando o novo saldo. Um saque não pode ocorrer 
+                ou se não houver saldo na conta, ou se o valor do saque for superior ao limite de 
+                saque da conta. Implemente a conta bancária conforme projeto abaixo:*/
+            try
+            {
+                Console.WriteLine("Enter account data:");
+                Console.Write("Number: ");
+                int AccNumber = int.Parse(Console.ReadLine());
+                Console.Write("Holder: ");
+                string holder = Console.ReadLine();
+                Console.Write("Initial Balance: ");
+                double initialBalance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("WithDraw limit: ");
+                double limit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Account acc = new Account(AccNumber, holder, initialBalance, limit);
+                Console.WriteLine();
+                Console.Write("Enter amount for withdraw: ");
+                double withDraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                acc.WithDraw(withDraw);
+                Console.WriteLine($"New Balance: {acc}");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($"Error. Limite de saque não disponivel. {e.Message}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
