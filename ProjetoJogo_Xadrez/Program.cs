0226eb8549b27c4795236a65cee12a18246eaf0f
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-05 20:38:44 -0300

    Atualização 20:38-05/04/2022-Add Class PosicaoXadrez

--- ProjetoJogo_Xadrez/Program.cs
+++ ProjetoJogo_Xadrez/Program.cs
@@ -15,15 +15,23 @@ namespace ProjetoJogo_Xadrez
                 - 2° sera a inteligencia do jogo
                 - 3° sera a camada de aplicação
             */
+
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+            Console.WriteLine(pos);
+
+            Console.WriteLine(pos.toPosicao());
+
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                #region Sem uso no momento
+                //Tabuleiro tab = new Tabuleiro(8, 8);
 
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                //tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                //tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                //tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-                Tela.ImprimirTabuleiro(tab);
+                //Tela.ImprimirTabuleiro(tab);
+                #endregion
 
                 Console.WriteLine($"");
             }
