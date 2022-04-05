10dd69273e4008016e19a3d4160b1b848bd21340
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-05 17:25:29 -0300

    Project Jogo de Xadrez Inicializado - Classe Tabuleiro Criada

---
+++ ProjetoJogo_Xadrez/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Tabuleiro;
+
+namespace ProjetoJogo_Xadrez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Este sistema ira utilizar de um sistema de camadas.
+                Serão tres camadas basicas;
+                - 1° sera o tabuleiro;
+                - 2° sera a inteligencia do jogo
+                - 3° sera a camada de aplicação
+            */
+
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine($"Posição {p}");
+
+            Console.ReadLine();
+        }
+    }
+}
