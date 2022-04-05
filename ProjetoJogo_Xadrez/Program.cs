ac8f97e4ed293a4e294c1e1442e4a0f8044210a6
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-05 18:27:45 -0300

    Atualização 18:27-05/04/2022-Add Classe(Rei,Torre)

--- ProjetoJogo_Xadrez/Program.cs
+++ ProjetoJogo_Xadrez/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using Xadrez;
 
 namespace ProjetoJogo_Xadrez
 {
@@ -17,6 +18,10 @@ namespace ProjetoJogo_Xadrez
 
 
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
             Tela.ImprimirTabuleiro(tab);
 
--- ProjetoJogo_Xadrez/Tabuleiro/Peca.cs
+++ ProjetoJogo_Xadrez/Tabuleiro/Peca.cs
@@ -8,11 +8,11 @@ namespace tabuleiro
         public int qteMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
-            this.cor = cor;
+            this.posicao = null;
             this.tab = tab;
+            this.cor = cor;
             qteMovimentos = 0;
         }
     }
