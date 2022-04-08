df63c720618028b060a89ca208ac612c58a5cb1e
Miller Vasco <millermartinsvasco1@gmail.com>
2022-04-08 12:23:48 -0300

    122308042022

---
+++ GetHashCodeEquals/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Entities;
+
+namespace GetHashCodeEquals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  
+             *  GetHashCode e Equals
+             *  
+                • São operações da classe Object utilizadas para comparar se um objeto é igual a outro
+                • Equals: lento, resposta 100%
+                • GetHashCode: rápido, porém resposta positiva não é 100%
+                • Os tipos pré-definidos já possuem implementação para essas operações. Classes e structs personalizados precisam sobrepô-las.*/
+
+            /*
+               Equals
+                Método que compara se o objeto é igual a outro, retornando true ou false.
+             */
+
+            #region Exemplo Equals
+            //Console.Write("Entre com um nome para A: ");
+            //string a = Console.ReadLine();
+            //Console.Write("Entre com um nome para B: ");
+            //string b = Console.ReadLine();
+            //Console.WriteLine($"A comparação de A e B é: {a.Equals(b)}");
+            #endregion
+
+            //Console.ReadLine();
+
+            /*
+              GetHashCode
+               Método que retorna um número inteiro representando um código gerado a partir das informações do objeto
+             */
+
+            #region Exemplo GetHashCode
+            Console.Write("Entre com um nome para 1: ");
+            string name1 = Console.ReadLine();
+            Console.Write("Entre com um Email para 1: ");
+            string email1 = Console.ReadLine();
+
+            Console.Write("Entre com um nome para 2: ");
+            string name2 = Console.ReadLine();
+            Console.Write("Entre com um Email para 2: ");
+            string email2 = Console.ReadLine();
+
+            Console.WriteLine();
+            Console.WriteLine("Se o numero GetHashCode for igual, siguinifica que o valor digitado é igual");
+            #endregion
+
+            Console.ReadLine();
+
+            /*
+             Regra de ouro do GetHashCode
+              • Se o código de dois objetos for diferente, então os dois objetos são diferentes
+              • Se o código de dois objetos for igual, muito provavelmente os objetos são iguais (pode haver colisão)
+             */
+
+            #region Programa Comparando Emails
+            Client A = new Client(name1, email1);
+            Client B = new Client(name2, email2);
+
+            if(A.Email == B.Email)
+            {
+                Console.WriteLine($"{A.Equals(B)}, {A.Email} é igual {B.Email}");
+                Console.WriteLine($"A: {A.GetHashCode()}, B: {B.GetHashCode()}");
+            }
+            else
+            {
+                Console.WriteLine($"{A.Equals(B)}, {A.Email} não é igual {B.Email}");
+                Console.WriteLine($"A: {A.GetHashCode()}, B: {B.GetHashCode()}");
+            }
+            #endregion
+
+        }
+    }
+}
---
+++ HashSetESortedSet/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashSetESortedSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             HashSet<T> e SortedSet<T>
+                • Representa um conjunto de elementos (similar ao da Álgebra) 
+                • Não admite repetições
+                • Elementos não possuem posição
+                • Acesso, inserção e remoção de elementos são rápidos
+
+                • Oferece operações eficientes de conjunto: interseção, união, diferença.
+            
+                • HashSet 
+                • https://msdn.microsoft.com/en-us/library/bb359438(v=vs.110).aspx
+
+                • SortedSet 
+                • https://msdn.microsoft.com/en-us/library/dd412070(v=vs.110).aspx
+
+            #####################################################################################################################
+
+            Diferenças
+                • HashSet 
+                • Armazenamento em tabela hash
+                • Extremamente rápido: inserção, remoção e busca O(1) 
+                • A ordem dos elementos não é garantida
+                • SortedSet 
+                • Armazenamento em árvore
+                • Rápido: inserção, remoção e busca O(log(n)) 
+                • Os elementos são armazenados ordenadamente conforme implementação  IComparer<T>
+
+            #####################################################################################################################
+
+            Alguns métodos importantes
+                • Add
+                • Clear 
+                • Contains
+                • UnionWith(other) - operação união: adiciona no conjunto os elementos do outro conjunto, sem repetição
+                • IntersectWith(other) - operação interseção: remove do conjunto os elementos não contidos em other 
+                • ExceptWith(other) - operação diferença: remove do conjunto os elementos contidos em other 
+                • Remove(T) 
+                • RemoveWhere(predicate)
+
+            #####################################################################################################################
+             */
+
+            //Demo1
+            #region Primeiro Exemplo Usando HashSet
+            Console.WriteLine("Primeiro Exemplo Usando HashSet");
+            HashSet<string> set = new HashSet<string>();
+            set.Add("TV");
+            set.Add("Notebook");
+            set.Add("Tablet");
+            Console.WriteLine(set.Contains("Notebook"));
+            foreach (String p in set)
+            {
+                Console.WriteLine(p);
+            }
+            Console.WriteLine();
+            #endregion
+
+            /*##################################################################################################################*/
+
+            //Demo2
+            #region Segundo exemplo Usando SortedSet
+            Console.WriteLine("Segundo Exemplo Usando SortedSet");
+            SortedSet<int> a = new SortedSet<int>() { 0, 2, 4, 5, 6, 8, 10 };
+            SortedSet<int> b = new SortedSet<int>() { 5, 6, 7, 8, 9, 10 };
+            //union
+            SortedSet<int> c = new SortedSet<int>(a);
+            c.UnionWith(b);
+            printCollection(c);
+            //intersection
+            SortedSet<int> d = new SortedSet<int>(a);
+            d.IntersectWith(b);
+            printCollection(d);
+            //difference
+            SortedSet<int> e = new SortedSet<int>(a);
+            e.ExceptWith(b);
+            printCollection(e);
+            #endregion
+
+            /*##################################################################################################################*/
+        }
+
+        //Demo2
+        #region Classe pertencente ao segundo exemplo
+        static void printCollection<T>(IEnumerable<T> collection)
+        {
+            foreach (T obj in collection)
+            {
+                Console.Write(obj + " ");
+            }
+            Console.WriteLine();
+        }
+        #endregion
+
+    }
+
+}
+
